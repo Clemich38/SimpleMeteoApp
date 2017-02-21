26017f59f9948dddda51df0eb4505260c597edfb
Clemich38 <clement.letreguilly@gmail.com>
2017-02-21 11:32:48 +0900

    remove empty line

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather-provider';
 import { WeatherDisplayPage } from '../weather-display/weather-display';
